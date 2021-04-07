5affb1983e5d9a3ab0456585d8cad3d96e9531d9
pirimb <pirimb@mail.ru>
2021-04-07 15:47:36 +0400

    ts added to the dialogsReducer .js->.ts

--- src/redux/authReducer.ts
+++ src/redux/authReducer.ts
@@ -4,19 +4,14 @@ import { authAPI, securityAPI } from "../api/api";
 const SET_AUTH_USER_DATA = 'auth/SET_AUTH_USER_DATA'
 const GET_CAPTCHA_URL_SUCCESS = 'auth/GET_CAPTCHA_URL_SUCCESS'
 
-type initialStateType = {
-    userId: number | null,
-    email: string | null,
-    login: string | null,
-    isAuth: boolean,
-    captchaUrl: string | null
-}
-let initialState: initialStateType = {
-    userId: null,
-    email: null,
-    login: null,
+type initialStateType = typeof initialState
+
+let initialState = {
+    userId: null as number | null,
+    email: null as string | null,
+    login: null as string | null,
     isAuth: false,
-    captchaUrl: null
+    captchaUrl: null as string | null
 }
 
 const authReducer = (state=initialState, action: any): initialStateType => {    
